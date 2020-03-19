30a56c66edafaff96362ec25401dd2e637a24faf
Christian Wischenbart <christian.wischenbart@gmail.com>
2020-03-19 18:56:38 +0100

    Simplify using statement

--- src/OpenMacroBoard.Examples.Minesweeper/Program.cs
+++ src/OpenMacroBoard.Examples.Minesweeper/Program.cs
@@ -1,5 +1,9 @@
 using OpenMacroBoard.Examples.CommonStuff;
+using StreamDeckSharp;
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenMacroBoard.Examples.Minesweeper
 {
@@ -7,11 +11,9 @@ namespace OpenMacroBoard.Examples.Minesweeper
     {
         private static void Main()
         {
-            using (var deck = ExampleHelper.OpenBoard())
-            {
-                using var wrapper = new MinesweeperDeckWrapper(deck, new ClassicMinesweeperIconSet(), 300);
-                ExampleHelper.WaitForKeyToExit();
-            }
+            using var deck = ExampleHelper.OpenBoard();
+            using var wrapper = new MinesweeperDeckWrapper(deck, new ClassicMinesweeperIconSet(), 300);
+            ExampleHelper.WaitForKeyToExit();
         }
     }
 }
