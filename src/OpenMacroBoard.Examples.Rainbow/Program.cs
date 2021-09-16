48c221e6589f0d192e14cc15a39475065a59013b
Christian Wischenbart <christian.wischenbart@dataformers.at>
2021-09-16 16:18:32 +0200

    Rainbow can be cleared now with board buttons.

--- src/OpenMacroBoard.Examples.Rainbow/Program.cs
+++ src/OpenMacroBoard.Examples.Rainbow/Program.cs
@@ -9,11 +9,18 @@ namespace OpenMacroBoard.Examples.Rainbow
         private static readonly Random rnd = new Random();
         private static readonly byte[] rgbBuffer = new byte[3];
 
+        private static int firstKeyId = 0;
+        private static int lastKeyId = 0;
+
+        private static bool firstKeyPressed = false;
+        private static bool lastKeyPressed = false;
+
         private static void Main()
         {
             using (var deck = ExampleHelper.OpenBoard())
             {
                 deck.SetBrightness(100);
+                lastKeyId = deck.Keys.Count - 1;
 
                 Console.WriteLine("INFO: Press some keys on the Stream Deck.");
                 deck.ClearKeys();
@@ -34,6 +41,21 @@ namespace OpenMacroBoard.Examples.Rainbow
             if (e.IsDown)
             {
                 d.SetKeyBitmap(e.Key, GetRandomColorImage());
+            }
+
+            if (e.Key == firstKeyId)
+            {
+                firstKeyPressed = e.IsDown;
+            }
+
+            if (e.Key == lastKeyId)
+            {
+                lastKeyPressed = e.IsDown;
+            }
+
+            if (firstKeyPressed && lastKeyPressed)
+            {
+                d.ClearKeys();
             }
         }
 
